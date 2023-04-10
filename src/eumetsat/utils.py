eb8f8fdddb3fbd66fa1d985f0f47e789de91b62e
Tim Cargan <timcargan@gmail.com>
2023-04-10 17:18:29 +0100

    Add type info
    Signed-off-by: Tim Cargan <timcargan@gmail.com>

--- src/eumetsat/utils.py
+++ src/eumetsat/utils.py
@@ -1,13 +1,13 @@
-import pyresample as pr
 from pyproj import CRS
+from pyresample.geometry import AreaDefinition
 
-from src.eumetsat import AREA_EXTENT, IMG_SIZE, TARGET_PROJ
+from eumetsat import AREA_EXTENT, IMG_SIZE, TARGET_PROJ
 
 
-def get_area_def():
+def get_area_def() -> AreaDefinition:
     area_extent = AREA_EXTENT
     area_id = "UK"
     proj_crs = CRS.from_user_input(TARGET_PROJ).to_dict()  # Target Projection EPSG:4326 standard lat lon geograpic
     output_res = IMG_SIZE  # Target res in pixels
-    area_def = pr.geometry.AreaDefinition.from_extent(area_id, proj_crs, output_res, area_extent)
+    area_def = AreaDefinition.from_extent(area_id, proj_crs, output_res, area_extent)
     return area_def
